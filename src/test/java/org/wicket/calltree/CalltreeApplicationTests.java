0ebd69204d7ce47ddd35b583a563802170e66533
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-04-03 16:41:45 +0100

    adding gitignore and initial project

---
+++ src/test/java/org/wicket/calltree/CalltreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wicket.calltree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalltreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
