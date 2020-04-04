b1f89bbc7b37b8f450cd9f517087a3a4930ce200
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-04-04 09:55:42 +0100

    Added Common module (empty). Added twilio response data class in kotlin (generated after twilio sends a successful message). TODO: refactor field names to naming convention and add JsonProperty

---
+++ calltree-commons/src/main/java/org/wicket/calltree/dto/PlaceholderClass.java
@@ -0,0 +1,8 @@
+package org.wicket.calltree.dto;
+
+/**
+ * @author Alessandro Arosio - 04/04/2020 09:48
+ */
+public class PlaceholderClass {
+    // just a placeholder for the package. Delete me afterwards
+}
