bb5a11b68c35be3f004b2ef2cef4a341bc4db38d
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-04-05 15:00:59 +0100

    Created "Entities" branch. Added repository module, Contact entity and DTO

---
+++ calltree-commons/src/main/java/org/wicket/calltree/dto/ContactDto.java
@@ -0,0 +1,25 @@
+package org.wicket.calltree.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wicket.calltree.enums.CallingOption;
+import org.wicket.calltree.enums.Role;
+
+import java.util.List;
+
+/**
+ * @author Alessandro Arosio - 04/04/2020 09:48
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private List<CallingOption> callingOption;
+    private Long pointOfContactId;
+}
