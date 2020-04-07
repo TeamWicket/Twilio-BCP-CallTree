b68eb66b2e191f988cd6f4b0e9e35b66d0102fef
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-04-07 22:42:28 +0100

    ContactController created. TODO tests (both mvc and service level)

---
+++ calltree-core/src/main/java/org/wicket/calltree/controllers/v1/ContactController.java
@@ -0,0 +1,46 @@
+package org.wicket.calltree.controllers.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.wicket.calltree.dto.ContactDto;
+import org.wicket.calltree.services.ContactService;
+
+import java.util.List;
+
+/**
+ * @author Alessandro Arosio - 07/04/2020 22:25
+ */
+@RestController
+@RequestMapping("/api/v1/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+    private final ContactService contactService;
+
+    @PostMapping
+    public ContactDto fetchContact(@RequestBody ContactDto contactDto) {
+        return contactService.getContact(contactDto);
+    }
+
+    @GetMapping("/all")
+    public List<ContactDto> fetchAllContacts() {
+        return contactService.getAllContacts();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeContact(@RequestBody ContactDto contactDto) {
+        contactService.deleteContact(contactDto.getId());
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDto saveContact(@RequestBody ContactDto contactDto) {
+        return contactService.saveOrUpdate(contactDto);
+    }
+
+    @PutMapping("/update")
+    public ContactDto updateContact(@RequestBody ContactDto contactDto) {
+        return contactService.saveOrUpdate(contactDto);
+    }
+}
