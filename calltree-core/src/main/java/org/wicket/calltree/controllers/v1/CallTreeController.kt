605534331ee1241bc5e8807b0e9b7a18c5849f96
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-04-15 16:37:26 +0100

    Added API endpoint specification

--- calltree-core/src/main/java/org/wicket/calltree/controllers/v1/CallTreeController.kt
+++ calltree-core/src/main/java/org/wicket/calltree/controllers/v1/CallTreeController.kt
@@ -1,5 +1,7 @@
 package org.wicket.calltree.controllers.v1
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -16,16 +18,19 @@ class CallTreeController(private val service: CallTreeService) {
 
   @PostMapping
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Initiate BCP calls")
   fun initiateCalls(@RequestBody @Valid bcpStartRequest: BcpStartRequest) {
     service.initiateCalls(bcpStartRequest)
   }
 
   @GetMapping
+  @Operation(summary = "Calculate stats")
   fun getStats() {
     // placeholder
   }
 
   @PostMapping("/twilio", produces = [MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE])
+  @Operation(summary = "Reply to incoming SMS (Twilio specific only)")
   fun replyToIncomingSms(@RequestBody body: String): String {
     return service.replyToSms(body)
   }
