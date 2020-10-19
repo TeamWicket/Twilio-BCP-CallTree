61173ebf7c118d068fa88e1b319f79f7b345d056
Vincent <lywc420@gmail.com>
2020-10-19 14:07:50 -0700

    Adds Wizard to create event

--- calltree-ui/src/Events.js
+++ calltree-ui/src/Events.js
@@ -3,7 +3,112 @@ import { List, Datagrid, TextField,
     DateField, BooleanField,
     SimpleForm, ReferenceField,
     TextInput, SelectInput, Create,
-    Toolbar, SaveButton, ReferenceInput, } from 'react-admin';
+    Toolbar, SaveButton, ReferenceInput} from 'react-admin';
+import {Step, StepLabel, Button, Typography, Stepper} from '@material-ui/core/';
+
+const Breadcrumb = props => {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+  
+    const handleNext = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+  
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+  
+    const handleReset = () => {
+      setActiveStep(0);
+    };
+  
+    return (
+      <div>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {activeStep === steps.length ? (
+            <div>
+              <h1>All steps completed</h1>
+              <Button onClick={handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              <Typography>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={handleBack}>
+                  Back
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleNext}>
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+  
+
+function getSteps() {
+    return ['Define event title', 'Create message body', 'Select roles','Twilio number'];
+}
+
+function getStepContent(stepIndex) {
+    switch (stepIndex) {
+        case 0:
+            return (
+            <div>
+                <h1>Select a title for the event</h1>
+                <TextInput source="eventName"  />
+            </div>)
+        case 1:
+            return (
+                <div>
+                    <h1>Message</h1>
+                    <p>Enter the message you wish to send.</p>
+                    <TextInput multiline source="text" />
+                </div>)
+        case 2:
+            return (
+                <div>
+                    <h1>Select the roles you wish to send this message to.</h1>
+                    <p>The message system is hierarchical<br />
+                        To send to everyone select reporter and it will be propogated up the chain.<br />
+                        If you wish for Leaders and everyone above then select this and so forth.<br />
+                        The Champion will not recieve a message as they will kick off the event.
+                    </p>
+                    <SelectInput source="toRoles" choices={[
+                        { id: 'MANAGER', name: 'Managers' },
+                        { id: 'LEADER', name: 'Leaders' },
+                        { id: 'REPORTER', name: 'Reporters'},
+                    ]}  />
+                </div>)
+        case 3:
+            return (
+                <div>
+                    <h1>Select a phone number</h1>
+            <ReferenceInput 
+                source="twilioNumberId" 
+                reference="numbers" 
+                filter={{ _getAvail: true}}> 
+                    <SelectInput optionText="twilioNumber" />
+            </ReferenceInput>
+        </div>
+            )
+        default:
+        return 'Unknown stepIndex';
+    }
+}
+
 
 const EventCreateToolbar = props => (
     <Toolbar {...props} >
@@ -29,19 +134,7 @@ export const EventList = props => (
 export const EventCreate = props => (
     <Create {...props}>
         <SimpleForm toolbar={<EventCreateToolbar />} redirect ="list">
-            <TextInput source="text" />
-            <SelectInput source="toRoles" choices={[
-                { id: 'MANAGER', name: 'Managers' },
-                { id: 'LEADER', name: 'Leaders' },
-                { id: 'REPORTER', name: 'Reporters'},
-            ]}  />
-            <TextInput source="eventName"  />
-            <ReferenceInput
-                source="twilioNumberId"
-                reference="numbers"
-                filter={{ _getAvail: true }}>
-                <SelectInput optionText="twilioNumber" />
-            </ReferenceInput>
+            <Breadcrumb {...props} />
         </SimpleForm>
     </Create>
-);
+)
